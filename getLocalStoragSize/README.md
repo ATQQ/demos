e7422f29b7b577b7847fecb0adf6f75840016af6
ATQQ <engineerzjl@foxmail.com>
2021-06-15 11:22:45 +0800

    docs: 更新示例

--- getLocalStoragSize/README.md
+++ getLocalStoragSize/README.md
@@ -5,9 +5,9 @@
 
 网上搜到的资料大多说的Chrome容量大小5M之类的，`LocalStorage`存储的内容格式为字符串
 
-试验了一下，这个5M实际上指的存储内容字符串长度，即`length`等于 **5*1024*1024**，并不是实际上的内容大小为5Mb
+试验了一下，这个5M实际上指的存储内容字符串长度，即`length`等于 **5\*1024*1024**，并不是实际上的内容大小为5Mb
 
-并且限制的是 key + value 的字符数不大于 5M(5*1024*1024)😁
+并且限制的是 key + value 的字符数不大于 5M(5\*1024*1024)😁
 
 ## 计算LocalStorage存储空间大小的方法
 
@@ -18,10 +18,18 @@
 
 结果如下：
 
-![图片](https://img.cdn.sugarat.top/mdImg/MTYyMzY4NTA3NTU4OQ==623685075589)
+![图片](https://img.cdn.sugarat.top/mdImg/MTYyMzcyNjgyNjM1MA==623726826350)
 
 ### 实际多浏览器测试
 使用[BrowserStack](https://live.browserstack.com/dashboard#os=Windows&os_version=7&browser=Chrome&browser_version=65.0&zoom_to_fit=true&full_screen=true&resolution=responsive-mode&url=https%3A%2F%2Fecom.meituan.com%2Fmeishi&speed=1),其提供了常见的各种浏览器环境，如下图所示
 
 ![图片](https://img.cdn.sugarat.top/mdImg/MTYyMzY4NDI3ODM1Mg==623684278352)
 
+### 测试结果
+>因为是 一次加 1M 进行测试可能存在误差
+
+**字符个数**
+* Firefox：5\*1024*1024
+* Chrome：5\*1024*1024
+* Opera:5\*1024*1024
+* IE8/11:4\*1024*1024
--- getLocalStoragSize/index.js
+++ getLocalStoragSize/index.js
@@ -1,25 +1,63 @@
+String.prototype.repeat = String.prototype.repeat || function (count) {
+    'use strict';
+    if (this == null) {
+        throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+        count = 0;
+    }
+    if (count < 0) {
+        throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+        throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+        return '';
+    }
+    // 确保 count 是一个 31 位的整数。这样我们就可以使用如下优化的算法。
+    // 当前（2014年8月），绝大多数浏览器都不能支持 1 << 28 长的字符串，所以：
+    if (str.length * count >= 1 << 28) {
+        throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (; ;) {
+        if ((count & 1) == 1) {
+            rpt += str;
+        }
+        count >>>= 1;
+        if (count == 0) {
+            break;
+        }
+        str += str;
+    }
+    return rpt;
+}
 function getStorageSize() {
-    const oneM = 1024 * 1024
-    let length = 0
+    var oneM = 1024 * 1024
+    var length = 0
     try {
-        for (let i = 0; i < 15; i++) {
+        for (var i = 0; i < 15; i++) {
             // 一次加1024*1024个字符
             length += oneM
             // 清空
             localStorage.clear()
-            const a = localStorage.setItem('a', '中'.repeat(length - 1))
+            var a = localStorage.setItem('a', '中'.repeat(length - 1))
         }
     } catch (error) {
         length -= oneM
     }
     localStorage.clear()
     // 得出最后结果
-    const en = new Blob(['a'.repeat(length)])
-    const zh = new Blob(['文'.repeat(length)])
+
     return [
-        `内容长度：${length}`,
-        `中文内容大小：${zh.size} 字节 ${formatSize(zh.size)}`,
-        `英文内容大小：${en.size} 字节 ${formatSize(en.size)}`
+        '内容长度：' + length,
+        'UTF-8(英文 - 1字节)内容大小：' + length + '字节' + formatSize(length),
+        'UTF-8(中文 - 3字节)内容大小：' + length * 3 + '字节' + formatSize(length * 3),
+        'UTF-16(中/英文 - 4字节)内容大小：' + length * 4 + '字节' + formatSize(length * 4),
     ]
 }
 
@@ -28,7 +66,7 @@ function getStorageSize() {
  * 来源于网络
  */
 function formatSize(size, pointLength, units) {
-    let unit
+    var unit
     units = units || ['B', 'K', 'M', 'G', 'TB']
     while ((unit = units.shift()) && size > 1024) {
         size /= 1024
@@ -36,6 +74,6 @@ function formatSize(size, pointLength, units) {
     return (unit === 'B' ? size : size.toFixed(pointLength === undefined ? 2 : pointLength)) + unit
 }
 
-const res = getStorageSize()
+var res = getStorageSize()
 
 document.getElementById('content').value = res.join('\n')
