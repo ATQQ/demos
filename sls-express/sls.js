8e5156b1efe59a914f960dc81c43d349cf28205b
sugar <engineerzjl@foxmail.com>
2022-05-04 21:16:16 +0800

    chore: express serverless demo 添加跨域配置

--- sls-express/sls.js
+++ sls-express/sls.js
@@ -2,6 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,token,cache-control')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  if(req.method === 'OPTIONS') {
+    res.end()
+    return
+  }
+  next()
+})
 // Routes
 app.get(`/`, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
